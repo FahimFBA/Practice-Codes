54eca50ce79456f2e1a77228b5eae16a27986c0f
FahimFBA <fahimbinamin@gmail.com>
2020-12-02 20:01:45 +0600

    ongoing java

---
+++ Java Codes/Addition.java
@@ -0,0 +1,11 @@
+
+
+public class Addition {
+    public static void main(String[] args){
+        int a=25;
+        int b=99;
+        int result=a+b;
+        System.out.println("The addition result is: " +  result);
+    }
+    
+}
---
+++ Java Codes/calculator.java
@@ -0,0 +1,18 @@
+
+
+public class calculator {
+    public static void main(String[] args){
+        int a=99;
+        int b=45;
+        int addition=a+b;
+        int substraction=a-b;
+        int division=a/b;
+        int multiplication=a*b;
+
+        System.out.println("Addition: "+ addition);
+        System.out.println("Substraction: "+ substraction);
+        System.out.println("Division: " + division);
+        System.out.println("Multiplication: " + multiplication);
+    }
+    
+}
