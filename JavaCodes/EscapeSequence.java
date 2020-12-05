f2e65ffded1ad39e7928f2499d40641ac1d654aa
FahimFBA <fahimbinamin@gmail.com>
2020-12-06 01:35:16 +0600

    printed inverted commas

--- JavaCodes/EscapeSequence.java
+++ JavaCodes/EscapeSequence.java
@@ -5,6 +5,7 @@ public class EscapeSequence {
         System.out.println("Fahim Bin Amin \nPhone number is: 01*********\nBSc in CSE");
         System.out.println("1\t2");
         System.out.println("3\t4");
+        System.out.println("\"I'm going to print the inverted commas here\"");
 
     }
     
