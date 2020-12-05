33b1870b32d071cdd6cf9af71aa26f313df2daa7
FahimFBA <fahimbinamin@gmail.com>
2020-12-06 01:26:21 +0600

    Started learning with Anisul Islam

--- JavaCodes/BioData.java
+++ JavaCodes/BioData.java
@@ -1,5 +1,6 @@
-package Java Codes;
-
 public class BioData {
-    
+    public static void main(String[] args){
+        System.out.println("Fahim Bin Amin");
+        System.out.println("Phone number is: 01*********");
+    }
 }
