0a19a96711b19effeadc1ad34a6db6e188a71467
FahimFBA <fahimbinamin@gmail.com>
2020-12-06 01:32:29 +0600

    Learned Escape Sequence

---
+++ JavaCodes/EscapeSequence.java
@@ -0,0 +1,11 @@
+package JavaCodes;
+
+public class EscapeSequence {
+    public static void main(String[] args){
+        System.out.println("Fahim Bin Amin \nPhone number is: 01*********\nBSc in CSE");
+        System.out.println("1\t2");
+        System.out.println("3\t4");
+
+    }
+    
+}
