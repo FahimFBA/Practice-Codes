e881a0f81fc99c308527a4eac4321282296d2028
FahimFBA <fahimbinamin@gmail.com>
2020-12-07 21:32:52 +0600

    added inputdemo

---
+++ JavaCodesFBA/LearnedWithAnisulIslam/InputDemo/InputDemo.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class InputDemo {
+    public static void main(String[] args){
+        Scanner input=new Scanner(System.in);
+        int number;
+        System.out.print("Enter any number : ");
+        number=input.nextInt();
+        System.out.println("The number = "+number);
+
+    }
+
+}
