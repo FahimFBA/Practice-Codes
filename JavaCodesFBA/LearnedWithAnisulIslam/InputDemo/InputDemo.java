3d5949923a0e75dd791bc48505f69a94cf371b00
FahimFBA <fahimbinamin@gmail.com>
2020-12-07 23:03:47 +0600

    edited input demo

--- JavaCodesFBA/LearnedWithAnisulIslam/InputDemo/InputDemo.java
+++ JavaCodesFBA/LearnedWithAnisulIslam/InputDemo/InputDemo.java
@@ -6,6 +6,19 @@ public class InputDemo {
         System.out.print("Enter any number : ");
         number=input.nextInt();
         System.out.println("The number = "+number);
+        String name = "Fahim Bin Amin";
+        System.out.println("Welcome "+name);
+
+        System.out.println("Now, enter your friend's name here: ");
+
+        /* friendsName=input.next(); input.next() can take only a single word from the user */
+        String friendsName=input.nextLine();
+        //input.nextLine() can take whole string from the user
+        System.out.println("We got the name of your friend! Welcome "+friendsName);
+        double num;
+        System.out.print("Enter a double number: ");
+        num=input.nextDouble();
+        System.out.println("The double number is: "+num);
 
     }
 
