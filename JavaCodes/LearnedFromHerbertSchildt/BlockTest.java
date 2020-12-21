d23ccff36f2b9e1055c5b8618a88264a7ba3dd52
FahimFBA <fahimbinamin@gmail.com>
2020-12-22 01:56:18 +0600

    two codes have been added

---
+++ JavaCodes/LearnedFromHerbertSchildt/BlockTest.java
@@ -0,0 +1,15 @@
+// Demonstrate a block of code
+public class BlockTest {
+    public static void main(String[] args){
+        int x , y;
+        y=20;
+        //the target of this loop is a block
+        
+        for ( x=0; x<10; x++){
+            System.out.println("This is x: "+x);
+            System.out.println("This is y: "+y);
+            y=y-2;
+        }
+    }
+    
+}
---
+++ JavaCodes/LearnedFromHerbertSchildt/ForTest.java
@@ -0,0 +1,9 @@
+class ForTest{
+    public static void main(String[] args){
+        int x;
+        for (x=0; x<10; x=x+1){  // here x is the loop control variable   
+            // this loop is also the same : for (x=0; x<10; x++)
+            System.out.println("This is x: "+x);
+        }
+    }
+}
