7a1a7cf74142f644294ca8c4ddbe79c639ea9165
FahimFBA <fahimbinamin@gmail.com>
2020-12-22 01:05:06 +0600

    Added IfSample

---
+++ JavaCodes/LearnedFromHerbertSchildt/Example2.java
@@ -0,0 +1,13 @@
+/* Here is another short example. 
+I'm calling this file "Example2.java"
+*/
+class Example2{
+    public static void main(String[] args){
+        int num; // this declares a variable called num  
+        num=100; //this assigns 100 as the value of num variable 
+        System.out.println("This is num: "+num);
+        num=num*2;
+        System.out.println("The value of num*2 is: "+num);
+        System.out.println(num);
+    }
+}
