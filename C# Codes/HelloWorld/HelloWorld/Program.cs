0ff22b41be60cfe0b393c1228309c5e77d597c76
FahimFBA <fahimbinamin@gmail.com>
2021-01-05 02:07:53 +0600

    HelloWorld in C#

---
+++ C# Codes/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.WriteLine("This is the first program in C#  !");
+            Console.ReadLine(); // the terminal would stay until I close that
+        }
+    }
+}
