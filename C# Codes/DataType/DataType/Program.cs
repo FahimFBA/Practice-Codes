b1784e51e29a37a4b4d92a0daf31b66ab720f64d
FahimFBA <fahimbinamin@gmail.com>
2021-01-05 02:22:30 +0600

    DataType

---
+++ C# Codes/DataType/DataType/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataType
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string phrase = "United International University";
+            // String is for more than one character
+            // String uses double quotation
+
+            char grade = 'A';
+            // Char is for only one character
+            // Char uses single quotation
+
+            int age = 20;
+
+            // you can use float, double and decimal for fraction
+            // float is least accurate/preciese and decimal is the most accurate
+
+            double GPA = 3.99;
+
+            bool isMale = true;
+
+            // bool variable can has only two values: true or false
+
+            Console.WriteLine(true);
+            Console.WriteLine("The age is " + age);
+
+            Console.ReadLine();
+
+        }
+    }
+}
