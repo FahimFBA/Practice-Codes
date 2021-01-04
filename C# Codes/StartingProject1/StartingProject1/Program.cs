5642e95c8bea62c4d7795f673f5809d9e626ded1
FahimFBA <fahimbinamin@gmail.com>
2021-01-04 23:52:45 +0600

    17.11 of C#

---
+++ C# Codes/StartingProject1/StartingProject1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StartingProject1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+
+
+            // Printing a simple pattern in the console 
+
+            Console.WriteLine("Printing a simple pattern here!");
+
+            Console.WriteLine("   /|");
+            Console.WriteLine("  / |");
+            Console.WriteLine(" /  |");
+            Console.WriteLine("/___|");
+
+            Console.ReadLine();
+            
+
+
+        }
+    }
+}
