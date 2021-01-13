3077f8634c597b41693f1877405b788b90c657a7
FahimFBA <fahimbinamin@gmail.com>
2021-01-13 21:19:17 +0600

    return statement in C#

---
+++ C# Codes/ReturnStatement/ReturnStatement/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReturnStatement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int cubedNumber = cube(5);
+            Console.WriteLine(cubedNumber);
+
+            Console.ReadLine();
+        }
+
+        static int cube(int num)// static for a new method, int is the returning data type 
+
+        {
+            int result = num * num * num;
+            return result;
+        }
+            
+     }
+        
+            
+}
