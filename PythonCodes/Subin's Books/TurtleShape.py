7069a599f968c02057d67e3294cd14fa9c75977e
FahimFBA <fahimbinamin@gmail.com>
2021-01-13 16:56:26 +0600

    shape in turtle

---
+++ PythonCodes/Subin's Books/TurtleShape.py
@@ -0,0 +1,21 @@
+import turtle #importing the turtle module
+
+turtle.shape("turtle") #defining the shape of the turtle
+
+# giving directions
+
+turtle.forward(200)
+turtle.left(90)
+
+turtle.forward(200)
+turtle.left(90)
+
+turtle.forward(200)
+turtle.left(90)
+
+turtle.forward(200)
+turtle.left(90)
+
+
+
+turtle.exitonclick() # terminal won't close automatically until the user wants to exit
