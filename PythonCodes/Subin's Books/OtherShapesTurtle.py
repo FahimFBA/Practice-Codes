15c043e9fe7e0c07904f67a9018b374ebca64a39
FahimFBA <fahimbinamin@gmail.com>
2021-01-13 17:02:42 +0600

    other shapes of turtle

---
+++ PythonCodes/Subin's Books/OtherShapesTurtle.py
@@ -0,0 +1,35 @@
+# For learning more: 
+# https://www.geeksforgeeks.org/turtle-shape-function-in-python/#:~:text=turtle.-,shape(),name%20of%20the%20current%20shape.&text=Shape%20with%20name%20must%20exist%20in%20the%20Turtle%20Screen's%20shape%20dictionary.
+# https://docs.python.org/3/library/turtle.html
+
+# import package 
+import turtle 
+
+
+# for default shape 
+turtle.forward(100) 
+
+# for circle shape 
+turtle.shape("circle") 
+turtle.right(60) 
+turtle.forward(100) 
+
+# for triangle shape 
+turtle.shape("triangle") 
+turtle.right(60) 
+turtle.forward(100) 
+
+# for square shape 
+turtle.shape("square") 
+turtle.right(60) 
+turtle.forward(100) 
+
+# for arrow shape 
+turtle.shape("arrow") 
+turtle.right(60) 
+turtle.forward(100) 
+
+# for turtle shape 
+turtle.shape("turtle") 
+turtle.right(60) 
+turtle.forward(100)
