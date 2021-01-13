92e442a919ebc51aacc210109013f3fd584c552d
FahimFBA <fahimbinamin@gmail.com>
2021-01-13 17:07:47 +0600

    Triangle shape in turtle

---
+++ PythonCodes/Subin's Books/TriangleTurtle.py
@@ -0,0 +1,9 @@
+import turtle
+turtle.shape("circle")
+turtle.speed(2) # defining the speed of the turtle module
+turtle.forward(100)
+turtle.left(120)
+turtle.forward(100)
+turtle.left(120)
+turtle.forward(100)
+turtle.left(120)
