3293477f830dcca162d16bd018d38c9140536015
FahimFBA <fahimbinamin@gmail.com>
2021-01-15 19:00:22 +0600

    if else in the C#

---
+++ C# Codes/IfStatement/IfStatement/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IfStatement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isMale = false;
+            bool isTall = true;
+
+            // bool isTall = false;
+
+            if (isMale && isTall) // && = and  ,  || = or 
+            {
+                Console.WriteLine("You are tall male!"); 
+            }
+
+            else if (isMale && !isTall) // ! indicates negation
+            {
+                Console.WriteLine("You are a short male.");
+            }
+
+            else if (!isMale && isTall)
+            {
+                Console.WriteLine("You are not male and not tall.");
+            }
+
+            else
+            {
+                Console.WriteLine("You are either not male or not tall or the both!");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
