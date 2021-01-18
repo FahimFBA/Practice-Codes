7155bab20b83ec8db5b6fc9c52e5e19c420b5f4f
FahimFBA <fahimbinamin@gmail.com>
2021-01-18 23:29:05 +0600

    datatypes c++

---
+++ C++ Codes/DataTypes.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    char grade = 'A';
+    string phrase = "FBA Academy";
+    int age = 20;
+    double gpa = 3.98;
+    bool isWoman = false;
+
+    cout << "The grade is: " << grade <<  endl;
+}
