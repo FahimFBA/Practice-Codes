501a733aed88a55331a26ca14f48b0b5a6d4b853
FahimFBA <fahimbinamin@gmail.com>
2021-01-18 23:21:16 +0600

    added variables

---
+++ C++ Codes/Variables.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    string CharacterName = "Fahim";
+    int CharacterAge = 20;
+    cout << "There once was a man named " << CharacterName << endl;
+    cout << "He was " << CharacterAge << " years old" << endl;
+    cout << "He liked the name " << CharacterName << endl;
+    cout << "But did not like being " << CharacterAge << " years old" << endl; 
+}
