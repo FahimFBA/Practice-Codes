a49ce38484412ab2c6904baae2351b755c171e84
FahimFBA <fahimbinamin@gmail.com>
2021-01-20 17:17:17 +0600

    while and do while loop in c#

---
+++ C# Codes/DoWhileLoop/DoWhileLoop/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoWhileLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int index = 6;
+            do // Do-while loop
+            {
+                Console.WriteLine(index);
+                index++;
+            } while (index <= 5); // do while execute the codes inside the loop before checking the condition
+            Console.ReadLine();
+        }
+    }
+}
