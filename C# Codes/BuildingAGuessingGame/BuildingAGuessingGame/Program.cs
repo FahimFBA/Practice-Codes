fbed36b0dd57357abc630a5a25d135709825a882
FahimFBA <fahimbinamin@gmail.com>
2021-01-21 20:41:58 +0600

    for loop in c#

---
+++ C# Codes/BuildingAGuessingGame/BuildingAGuessingGame/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuildingAGuessingGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string secretWord = "Fahim";
+            string guess = "";
+            int guessCount = 0;
+            int guessLimit = 3; // providing a limit of guessing
+            bool outOfGuess = false;  // out of guess = true
+
+            while (guess != secretWord && !outOfGuess)
+            {
+                if (guessCount < guessLimit)
+                {
+                    Console.Write("Enter your guessed word: ");
+                    guess = Console.ReadLine();
+                    guessCount++; // incrementing the count
+                }
+
+                else // out of the guess limit
+                {
+                    outOfGuess = true; // flagging
+                }
+                
+            }
+
+            if (outOfGuess) // if the user can't win within the provided limit
+            {
+                Console.Write("Ops! You LOST!");
+            }
+            else // if the user win within the provided limit
+            {
+                Console.Write("Congratulations! You won!!!");
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}
---
+++ C# Codes/ForLoops/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ForLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            while (i <= 5) // while loop
+            {
+                Console.WriteLine(i);
+                i++; // increment
+            }
+
+            for (int j = 1; j <= 5; j++) // for loop 
+            {
+                Console.WriteLine(j); // structure of for loop (initialization ; condition ; increment/decrement)
+            }
+
+            Console.WriteLine("--------------------------------------------");
+
+            int[] luckyNumbers = { 4, 8, 15, 16, 23, 42 };
+
+            for ( int k = 0 ; k < luckyNumbers.Length ; k++ ) // the loop would run till the last indext of the length
+            {
+                Console.WriteLine(luckyNumbers[k]);
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
