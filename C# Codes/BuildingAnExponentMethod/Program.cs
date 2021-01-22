a87d731b4cccf8dd8072f8f6f08da4f728bb8f7c
FahimFBA <fahimbinamin@gmail.com>
2021-01-22 22:21:11 +0600

    exponent method

---
+++ C# Codes/BuildingAnExponentMethod/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace BuildingAnExponentMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetPow(4, 3));
+            Console.ReadLine();
+        }
+
+        static int GetPow(int baseNum, int powerNum)
+        {
+            int result = 1; // initialization
+
+            for (int i = 0 ; i < powerNum ; i++) // for loop
+            {
+                result = result * baseNum;
+            }
+            return result;
+        }
+    }
+}
