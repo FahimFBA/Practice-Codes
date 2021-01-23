212cd836359110ee50c23f821f38ee7f52d730c6
FahimFBA <fahimbinamin@gmail.com>
2021-01-24 01:21:11 +0600

    2D Arrays

---
+++ C# Codes/2DArrays/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _2DArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] numberGrid = {  // int[,] is telling c# that we want a two dimensional array here. For there dimensional array, use [, ,]
+                {1, 2}, // 0,0 indicates 1 whereas 0,1 indicates 2
+                {3 , 4}, // 1,0 indicates 3 whereas 1,1 indicates 4
+                {5, 6} // 2,0 indicates 5 whereas 2,1 indicates 6
+
+            };
+            Console.WriteLine(numberGrid[2,1]); // Accessing the elements of the array
+            
+            int[,] MyArray = new int[2,3]; // row = 2 and column = 3
+
+            Console.ReadLine();
+        }
+    }
+}
