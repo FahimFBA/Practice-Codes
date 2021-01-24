40f5f2cbb8259687314d86ac7c6c0d13f0495570
FahimFBA <fahimbinamin@gmail.com>
2021-01-24 16:09:54 +0600

    added comment

---
+++ C# Codes/Comments/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Comments
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // comments are little line or block of code which get ignored
+            Console.WriteLine("Comments are fun");
+            Console.WriteLine("The next line won't be printed as that line is a comment");
+            // it's an example of single line comment
+
+
+            /*
+
+
+            This is a sample of multi-line comment
+
+
+            */
+            Console.ReadLine();
+        }
+    }
+}
