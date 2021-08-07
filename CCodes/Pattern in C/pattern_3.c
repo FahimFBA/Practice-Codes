7b0dc57c0f601c858df3da659485acfcd35aa741
FahimFBA <fahimbinamin@gmail.com>
2021-08-07 22:09:48 +0600

    pattern 5 of Anisul Islam - Unfinished

---
+++ CCodes/Pattern in C/pattern_3.c
@@ -0,0 +1,24 @@
+
+// Pattern 5
+#include<stdio.h>
+int main()
+{
+    int n, row, col;
+    scanf("%d", &n);
+    for(row=0; row<n; row++)
+    {
+        for(col=0; col<row; col++)
+        {
+            if (col % 2 != 0)
+            {
+                printf("%c" , );
+            }
+
+            else
+            {
+                printf("B");
+            }
+            
+        }
+    }
+}
