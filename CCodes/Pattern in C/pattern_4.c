55cb299d346115cae70573e2ec3fd2a349a8ee48
FahimFBA <fahimbinamin@gmail.com>
2021-08-07 22:12:32 +0600

    solved pattern 4 of Anisul Islam

---
+++ CCodes/Pattern in C/pattern_4.c
@@ -0,0 +1,34 @@
+/*
+
+Output: for n =3
+
+1
+00
+111
+
+
+
+*/
+
+#include <stdio.h>
+int main()
+{
+    int n, i, j;
+    scanf("%d", &n);
+    for (i = 1; i <= n; i++)
+    {
+        for (j = 1; j <= i; j++)
+        {
+            if (i % 2 != 0)
+            {
+                printf("1");
+            }
+            else
+            {
+                printf("0");
+            }
+            
+        }
+        printf("\n");
+    }
+}
