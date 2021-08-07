79ce3207a53ca8d1f184765a572461fe0abdd532
FahimFBA <fahimbinamin@gmail.com>
2021-08-08 00:03:17 +0600

    solved patter 5 from Anisul Islam

---
+++ CCodes/Pattern in C/pattern_6.c
@@ -0,0 +1,28 @@
+/*
+
+Output: for n =3
+
+A
+AB
+ABC
+
+
+
+*/
+
+#include <stdio.h>
+int main()
+{
+    int n, i, j;
+    scanf("%d", &n);
+    for (i = 1; i <= n; i++)
+    {
+        for (j = 1; j <= i; j++)
+        {
+          
+            printf("%c" , 64+j);
+            
+        }
+        printf("\n");
+    }
+}
