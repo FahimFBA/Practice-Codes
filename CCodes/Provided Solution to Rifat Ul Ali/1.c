4f7b2a732fdb3060811f39624c36a7c752ac026f
FahimFBA <fahimbinamin@gmail.com>
2021-10-06 14:27:48 +0600

    shifting from laptop

--- CCodes/Provided Solution to Rifat Ul Ali/1.c
+++ CCodes/Provided Solution to Rifat Ul Ali/1.c
@@ -1,9 +1,20 @@
 #include <stdio.h>
+#include<stdlib.h>
 
 void strcat(char *str1, char *str2);
 
 int main()
 {
+    FILE *fptr;
+
+    if ((fptr = fopen("program.txt", "r")) == NULL)
+    {
+        printf("Error! opening file");
+
+        // Program exits if the file pointer returns NULL.
+        exit(1);
+    }
+
     char str1[100], str2[100];
     scanf("%s", &str1);
     scanf("%s", &str2);
