2ef8974eceb7663c08100410ce05d3331d7f6b3f
FahimFBA <fahimbinamin@gmail.com>
2022-08-19 22:54:59 +0600

    solved the first one

---
+++ CCodes/Provided Solution to Asaduzzaman Khaled/1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    float b;
+    scanf("%d%f" , &a, &b);
+    printf("Integer value = %d\n" , a);
+    printf("Floating point value = %.3f\n" , b);
+    return 0;
+}
