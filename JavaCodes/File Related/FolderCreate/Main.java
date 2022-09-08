c7953735114e6759bf65d2b124c2e0732c1e370e
FahimFBA <fahimbinamin@gmail.com>
2022-09-08 11:10:01 +0600

    folder creation and deletion

---
+++ JavaCodes/File Related/FolderCreate/Main.java
@@ -0,0 +1,19 @@
+import java.io.File;
+
+public class Main{
+    public static void main(String[] args) {
+        File file = new File("C://Users//Md. Fahim Bin Amin//Desktop//Practice-Codes//JavaCodes//File Related//CreatedFolder");
+        if (file.mkdir()) {
+            System.out.println("Folder created");
+        } else {
+            System.out.println("Folder not created");
+        }
+        String directoryLocation = file.getAbsolutePath();
+        System.out.println(directoryLocation);
+        if(file.delete()) {
+            System.out.println("Folder deleted");
+        } else {
+            System.out.println("Folder not deleted");
+        }
+    }
+}
