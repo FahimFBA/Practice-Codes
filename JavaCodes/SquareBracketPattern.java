d63cfe0af730fe891e54bc3225487e4ff0bf7d44
FahimFBA <fahimbinamin@gmail.com>
2022-10-03 10:38:29 +0600

    sqaure bracket pattern in java

---
+++ JavaCodes/SquareBracketPattern.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class SquareBracketPattern {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int rows = scanner.nextInt();
+        for (int i = 1; i <= rows; ++i) {
+            for (int j = 1; j <= i; ++j) {
+                System.out.print("[][]");
+            }
+            System.out.println();
+        }
+    }
+}
